982f6cf0344a563d245a1a8614936f134e5cc13b
maxim <maxim@webro.com.ua>
2018-09-18 20:51:19 -0700

    small fixes in master

--- redux-todo/src/components/Todo.js
+++ redux-todo/src/components/Todo.js
@@ -1,10 +1,10 @@
-import React from 'react'
-import PropTypes from 'prop-types'
+import React from "react";
+import PropTypes from "prop-types";
 
 const Todo = ({onClick, completed, text}) => (
 	<li onClick={onClick}
 		style={{textDecoration: completed ? 'line-through' : 'none'}}>
-	{text}
+		{text}
 	</li>
 );
 
